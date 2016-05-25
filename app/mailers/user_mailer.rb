626093cf2249c432077e636e8b5e41c75635872e
alex <alexandramaecoleman@gmail.com>
2016-05-25 20:11:15 +0100

    mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         :to => 'alexandramaecoleman@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+  
 end
