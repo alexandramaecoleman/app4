481b708fe81e9152ba926e6400e60ada1ac5a7e3
alex <alexandramaecoleman@gmail.com>
2016-06-01 11:30:51 +0100

    welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,10 +8,10 @@ class UserMailer < ApplicationMailer
         :subject => "A new contact form message from #{name}")
   end
 
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  def welcome(user)
+    @appname = "Mae Shop"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
   end
   
 end
