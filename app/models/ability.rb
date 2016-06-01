85e0136ccb167f6bc3cbd70d17b306081627c984
alex <alexandramaecoleman@gmail.com>
2016-06-01 09:52:47 +0100

    ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,15 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    cannot [:create, :edit, :update, :destroy] [Product]
   end
 
   def initialize(user)
     user ||= admin # admin
-    can [:update, :destroy] [Product, Comment]
+    can [:read, :create, :update, :destroy] [Product, Comment]
+  else
+  	can [:create] [Comment]
+  	cannot [:create, :edit, :update, :destroy] [Product]
   end
 
 end
