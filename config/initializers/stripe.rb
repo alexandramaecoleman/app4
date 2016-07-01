c0df88d2cdb119c9b1a5ad6e9f8b3ed154a5dbd2
alex <alexandramaecoleman@gmail.com>
2016-07-01 14:27:03 +0100

    stripe

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_3tgD79h7O8ELWA6wPLBFEbAV Roll Key',
+    :secret_key => 'sk_test_CQwJtkfbnFk25Os83jqouhVz'
+  }
+end
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
